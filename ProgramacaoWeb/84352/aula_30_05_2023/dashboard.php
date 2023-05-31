55caa9883cdbaf06b18fee3271cdde0be17a2619
Yago Accioly <125842762+YagoAcc@users.noreply.github.com>
2023-05-30 21:07:30 -0300

    Add files via upload

---
+++ ProgramacaoWeb/84352/aula_30_05_2023/dashboard.php
@@ -0,0 +1,34 @@
+<?php
+
+session_start();
+ob_start();
+
+require_once "connect.php";
+
+if ( $_SESSION['loginOk'] != true) {
+    header('Location: index.php');
+    die();
+}
+if (isset($_GET['exit'])) {
+    session_destroy();
+    header('Location: index.php');
+    die();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Dashboard</h1>
+    <h2>Olá, <?=  $_SESSION['name']; ?></h2>
+    <p>
+        <a href="?exit">Deslogar</a>
+    </p>
+</body>
+</html>
